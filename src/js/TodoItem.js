0e220f736f30726816c96d8332db66fed3f111c5
Sergio Peña <sergiop79@gmail.com>
2018-01-28 13:25:01 +0100

    fix indentation

--- src/js/TodoItem.js
+++ src/js/TodoItem.js
@@ -1,9 +1,9 @@
 export default class TodoItem {
-    constructor (name) {
-      this.id = new Date().getTime()
-      this.name = name
-      this.isComplete = false
-      this.isActive = true
-      return this
-    }
+  constructor (name) {
+    this.id = new Date().getTime()
+    this.name = name
+    this.isComplete = false
+    this.isActive = true
+    return this
+  }
 }
